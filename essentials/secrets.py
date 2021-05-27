3faea6a5f28c1ac42b2f9b1973dac0d0b3c2de23
Salvi0 <66015217+Salvi0@users.noreply.github.com>
2021-05-27 09:16:26 +0200

    Update secrets.py

--- essentials/secrets.py
+++ essentials/secrets.py
@@ -2,7 +2,7 @@ class Secrets:
     def __init__(self):
         self.dbl_token = ''  # DBL token (only needed for public bot)
         self.mongo_db = 'mongodb+srv://modmail:luna@cluster0.wlkwe.mongodb.net/'
-        self.bot_token = 'NjkyODI0NTQ2MjQzNTEwNDMy.Xn0JIw.ySHCu_8ik8jzCzJp9TOs80z8nIs' # official discord bot token
+        self.bot_token = 'NjQ0MjY0NTk4OTcwODI2NzUy.XcxgKg.kVSA-Xm2O-WiC8G16GWKpQkda2I' # official discord bot token
         self.mode = 'development' # or production
 
 SECRETS = Secrets()
